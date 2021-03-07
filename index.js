dc156ec0065e9c33479da34dde6b3c8b2f699233
Tautvydas <banys.tautvydas@gmail.com>
2021-03-07 18:06:29 +0200

    Clears console

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 #!/usr/bin/env node
 
+const clearConsole = require('clear-any-console');
+
+// Clears the console.
+clearConsole();
+
 // Shabang in to of the top of the file
 // It tells terminal to run this script with node.js
 console.log(`Tautvydas Banys`);

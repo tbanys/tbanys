685d0f9e6b74a1c4dbc9c7046cc56c356a036ab9
Tautvydas <banys.tautvydas@gmail.com>
2021-03-07 21:22:39 +0200

    welcome header

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 #!/usr/bin/env node
 
-const clearConsole = require('clear-any-console');
+const pkgJSON = require('./package.json');
+const welcome = require('cli-welcome');
 
-// Clears the console.
-clearConsole();
+welcome({
+  title: pkgJSON.name,
+  tagLine: 'Hello!',
+  description: pkgJSON.description,
+  version: pkgJSON.version,
+  bgColor: `#FADC00`,
+	color: `#000000`,
+	bold: true,
+	clear: true,
+});
 
 // Shabang in to of the top of the file
 // It tells terminal to run this script with node.js
-console.log(`Tautvydas Banys`);
+console.log(`
+  Tautvydas Banys
+
+  Web developer
+`);

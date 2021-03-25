c90e46904361b8e5659b1e52839f30f4b35593d4
Tautvydas <banys.tautvydas@gmail.com>
2021-03-25 11:06:22 +0200

    added moew and help text

--- index.js
+++ index.js
@@ -1,13 +1,53 @@
 #!/usr/bin/env node
+const meow = require('meow');
 const handleError = require('cli-handle-error');
 
 const init = require('./utils/init');
 const data = require('./utils/data');
 
+const helpText = `
+  Usage
+    npx tbanys [options]
+
+  Options
+    bio         Show bio information
+    --no-bio    Don't Show bio information
+    alerts      Show alerts information
+    --no-alers  Don't show alerts information
+
+  Examples
+    npx tbanys --no-social
+`;
+
+const options = {
+  flags: {
+    bio: {
+      type: 'boolean',
+      default: true
+    },
+    alerts: {
+      type: 'boolean',
+      default: true
+    }
+  }
+}
+const cli = meow(helpText, options);
+const flags = cli.flags;
+const inputs = cli.inputs;
+
 (() => {
   init();
-  console.log(data.bio);
-  console.log(data.alerts);  
-  const err = new Error(`Something went wrong`);
-  handleError(`BLOG IS DOWN`, err, false, false);
+  if (flags.bio) {
+    console.log(data.bio);
+  }
+
+  if (flags.alerts) {
+    console.log(data.alerts);  
+  }
+
+  console.log(`cli.input`, cli.input);
+  console.log(`cli.flags`, cli.flags);
+
+  // const err = new Error(`Something went wrong`);
+  // handleError(`BLOG IS DOWN`, err, false, false);
 })();
